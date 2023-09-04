32b87faf7aa59c402b4e1f124c06ba086a9f6a6f
ReaMart <126715517+Rea-Mart@users.noreply.github.com>
2023-09-04 08:15:28 +0200

    log update, pre-restructure

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 from hisock import *
 import hisock
 server = hisock.server.ThreadedHiSockServer((hisock.get_local_ip(), 6969))
+min_clients = 2
 
 @server.on("join")
 def on_client_join(client: hisock.utils.ClientInfo):
     server.send_client(client, "ask_profile", "Do you like sheep?")
-
+    client_count = len(server.get_all_clients())
+    server.send_all_clients("server_msg",f"{client.name} connected ({client_count}/{min_clients})")
+    if client_count >= min_clients:
+        server.send_all_clients("server_msg","enough clients")
+        server.send_all_clients("gamestate","play")
+        print("enough players")
 @server.on("client_profile")
 def on_question_response(client: hisock.utils.ClientInfo, profile:dict):
     print(profile)
